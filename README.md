2b0e974f0b1f96ad8906db31f8e8a38ed689fdd8
David Ku <hola@davidku.dev>
2022-07-08 16:37:11 -0500

    update readme and file .sh

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# install-docker-in-serverdiazar
-Docker installation, container deployment with configured image.
+# Install docker and deploy container on Diazar server with Debian 10
+
+The script will uninstall any previously installed docker, install docker and run a container named "serverdz".
+
+This container uses the latest image [davidkudev/server-diazar](https://hub.docker.com/r/davidkudev/server-diazar) and will be deployed to the port 3306. The container shares the directory `/var/lib/mysql` with the host at `~/backupDB`.
+
+The image [davidkudev/server-diazar](https://hub.docker.com/r/davidkudev/server-diazar) has the following features:
+
+## Features
+
+- O.S: Debian 10
+- Database: MySQL 5.7 _user: root, password: merida_
+- No-IP - ready to configure with credentials.
+- Expose port: 3306
+
+## Instructions
+
+1. Confirm you are in your user directory `cd ~`
+2. Clone this repository.
+
+   ```bash
+    git clone https://github.com/daveku/install-docker-in-serverdiazar.git
+   ```
+
+3. Access the repository.
+
+   ```bash
+    cd install-docker-in-serverdiazar
+   ```
+
+4. Run the following command to install docker in serverdiazar.
+
+   ```bash
+   ./deploy_docker.sh
+   ```
--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,46 +1,37 @@
 #!/bin/bash
 echo Starting processes
-echo 
-echo
-echo ---[1] check updates to linux
+
+
+echo ---> [1] check updates to linux
 sudo apt update && sudo apt upgrade -y
-echo 
-echo
-echo ---[2] Older versions of Docker were called docker, docker.io, or docker-engine. If these are installed, uninstall them:
+
+echo ---> [2] Older versions of Docker were called docker, docker.io, or docker-engine. If these are installed, uninstall them:
 sudo apt-get remove docker docker-engine docker.io containerd runc
-echo 
-echo
-echo ---[3] Install packages to allow apt to use a repository over HTTPS:
+
+echo ---> [3] Install packages to allow apt to use a repository over HTTPS:
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-echo 
-echo
-echo ---[4] Add Docker’s official GPG key:
+    
+echo ---> [4] Add Docker’s official GPG key:
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo y
-echo 
-echo
-echo ---[5] set up the repository:
+
+echo ---> [5] set up the repository:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo 
-echo
-echo ---[6] Install docker
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-echo 
-echo
-echo ---[7] Create container in docker
+  
+echo ---> [6] Install docker
+sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+echo ---> [7] Create container in docker
 sudo docker run -d --name serverdz -p 3306:3306 -v ~/backupDB:/var/lib/mysql davidkudev/server-diazar
-echo 
-echo
-echo ---[8] Accessing the container
+
+echo ---> [8] Accessing the container
 sudo docker exec -it serverdz bash
-echo 
-echo 
+
 echo Processes finished
