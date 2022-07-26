adcb3f8f3a0609e400a4894262c2b7bef8db51e2
David Ku <hola@davidku.dev>
2022-07-26 12:52:17 -0500

    Update deploy_docker.sh

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -2,33 +2,33 @@
 echo Starting processes
 
 
-echo ---> [1] check updates to linux
+echo ---> 1 check updates to linux
 sudo apt update && sudo apt upgrade -y
 
-echo ---> [2] Older versions of Docker were called docker, docker.io, or docker-engine. If these are installed, uninstall them:
+echo ---> 2 Older versions of Docker were called docker, docker.io, or docker-engine. If these are installed, uninstall them:
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
-echo ---> [3] Install packages to allow apt to use a repository over HTTPS:
+echo ---> 3 Install packages to allow apt to use a repository over HTTPS:
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
     
-echo ---> [4] Add Docker’s official GPG key:
+echo ---> 4 Add Docker’s official GPG key:
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo y
 
-echo ---> [5] set up the repository:
+echo ---> 5 set up the repository:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
-echo ---> [6] Install docker
+echo ---> 6 Install docker
 sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-echo ---> [7] Create container in docker
+echo ---> 7 Create container in docker
 sudo docker run -d --name diazarserver -p 3306:3306 -v /root/backupDB:/var/lib/mysql daveku/diazar-server
 
 echo Servidor Diazar corriendo
